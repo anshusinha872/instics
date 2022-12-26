085d76bf2bafa7a3e2ee81b43ae3e2a941a2512f
ChittoX <77602685+HARSHCHITTORA@users.noreply.github.com>
2022-12-26 18:54:22 +0530

    Update app-routing.module.ts

--- UI/user/src/app/app-routing.module.ts
+++ UI/user/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './view/pages/login/login.component';
+import { SignupComponent } from './view/pages/signup/signup.component';
+const routes: Routes = [
+  { path: '', redirectTo: 'LoginComponent', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  
+  {path:'dashboard',component:DashboardComponent},
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },
+  // {
+  //   path: 'login', children: [
+  //     { path: 'signup', component: SignupComponent },
+  //   ]
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
