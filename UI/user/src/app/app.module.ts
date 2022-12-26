eedeeff3bd0745cd3a91c8d5092e7a9c7daa24c6
ChittoX <77602685+HARSHCHITTORA@users.noreply.github.com>
2022-12-26 18:52:25 +0530

    update app.module.ts

--- UI/user/src/app/app.module.ts
+++ UI/user/src/app/app.module.ts
@@ -1,28 +1,66 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './Views/pages/login/login.component';
-import { SignUpComponent } from './Views/pages/sign-up/sign-up.component';
-import { HeaderComponent } from './Views/partial/header/header.component';
-import { FooterComponent } from './Views/partial/footer/footer.component';
-import { SideBarComponent } from './Views/partial/side-bar/side-bar.component';
+import {ButtonModule} from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import {SliderModule} from 'primeng/slider';
+import { HeaderComponent } from './view/partial/header/header.component';
+import { FooterComponent } from './view/partial/footer/footer.component';
+import { LoginComponent } from './view/pages/login/login.component';
+import {PasswordModule} from 'primeng/password';
+import {DividerModule} from 'primeng/divider';
+import { FormsModule } from '@angular/forms' 
+import { ErrorComponent } from './view/pages/error/error.component';
+import { SignupComponent } from './view/pages/signup/signup.component';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import { HomeComponent } from './view/pages/home/home.component';
+import { SideBannerComponent } from './view/partial/side-banner/side-banner.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {AngularFireModule} from '@angular/fire/compat'
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from 'src/environments/environment.prod';
+import { NgOtpInputModule } from 'ng-otp-input';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignUpComponent,
     HeaderComponent,
     FooterComponent,
-    SideBarComponent
+    LoginComponent,
+    ErrorComponent,
+    SignupComponent,
+    HomeComponent,
+    SideBannerComponent,
+    HomeComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    SliderModule,
+    PasswordModule,
+    DividerModule,
+    FormsModule,
+    PasswordModule,
+    ConfirmDialogModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    FormsModule,
+    NgOtpInputModule
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+
 })
 export class AppModule { }
