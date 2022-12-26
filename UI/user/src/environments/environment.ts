78b7a63a2fc09d4b20f02323e0e5c1290fa20f28
ChittoX <77602685+HARSHCHITTORA@users.noreply.github.com>
2022-12-26 19:56:08 +0530

    Update environment.ts

--- UI/user/src/environments/environment.ts
+++ UI/user/src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCPDGJBgD8DLl8f3483pxHfSzJJ92I8fpo",
+  authDomain: "otp-verification-dca78.firebaseapp.com",
+  projectId: "otp-verification-dca78",
+  storageBucket: "otp-verification-dca78.appspot.com",
+  messagingSenderId: "912597418594",
+  appId: "1:912597418594:web:b676956ef2482c4820073b"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
