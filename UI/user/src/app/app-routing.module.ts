b90ad68c19e82eecb115751a48e147218204d565
ChittoX <77602685+HARSHCHITTORA@users.noreply.github.com>
2022-12-26 19:54:29 +0530

    Update app-routing.module.ts

--- UI/user/src/app/app-routing.module.ts
+++ UI/user/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './view/pages/login/login.component';
-import { SignupComponent } from './view/pages/signup/signup.component';
+import { LoginComponent } from './Views/pages/login/login.component';
+import { SignupComponent } from './Views/pages/sign-up/sign-up.component';
 const routes: Routes = [
   { path: '', redirectTo: 'LoginComponent', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
