e30303a15f954bd2b159e033858e3a2051fe185c
ChittoX <77602685+HARSHCHITTORA@users.noreply.github.com>
2022-12-26 19:56:31 +0530

    Update environment.prod.ts

--- UI/user/src/environments/environment.prod.ts
+++ UI/user/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCPDGJBgD8DLl8f3483pxHfSzJJ92I8fpo",
+  authDomain: "otp-verification-dca78.firebaseapp.com",
+  projectId: "otp-verification-dca78",
+  storageBucket: "otp-verification-dca78.appspot.com",
+  messagingSenderId: "912597418594",
+  appId: "1:912597418594:web:b676956ef2482c4820073b"
+  }
 };
